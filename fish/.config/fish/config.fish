6c397521d812cbcc406f06ea6ed2a765ec402be4
eleventozero <christiansauerwald@posteo.de>
2026-05-05 23:00:27 +0200

    add fish basic config and aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,25 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # BASIC SETTINGS
+    set -g fish_greeting ""
+
+    # ==========================================
+    # ALIASES
+    # ==========================================
+
+    # -------- Navigation --------
+    alias ..="cd .."
+    alias ...="cd ../.."
+
+    # -------- Listing --------
+    alias ll="ls -lh"
+    alias la="ls -a"
+
+    # -------- Git --------
+    alias ga="git add"
+    alias gc="git commit"
+    alias gp="git push"
+
+    # Override ghostscript (gs)
+    functions -e gs 2>/dev/null
+    alias gs="git status"
 end
